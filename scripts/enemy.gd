44470306bbc2452700854ac801421c38212100ed
beatmil <anutorn@gmail.com>
2024-09-04 22:20:19 +0100

    Enemy now attack player when in range

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,11 +3,17 @@ extends "res://scripts/base_character.gd"
 @export var target: CharacterBody2D
 
 
+enum {
+	FOLLOW,
+	}
+
+
 #############################################################
 ## Config
 #############################################################
 # var is_face_right:bool = true
-var SPEED:int = 100
+var SPEED: int = 100
+var is_player_in_range: bool = false
 
 
 func _physics_process(_delta: float) -> void:
@@ -15,22 +21,26 @@ func _physics_process(_delta: float) -> void:
 	_z_index_equal_to_y()
 	_move(_delta)
 	_facing()
+	if is_player_in_range:
+		_lp()
 
 
 func _ready() -> void:
+	pass
 	# $Timer.start()
-	_lp()
+	# _lp()
 
 
 #############################################################
 ## Private Function
 #############################################################
 func _move( delta):
-	var direction = (target.position - global_position).normalized() 
-	var desired_velocity =  direction * SPEED
-	var steering = (desired_velocity - velocity) * delta * 2.5
-	velocity += steering
-	move_and_slide()
+	if state == States.IDLE:
+		var direction = (target.position - global_position).normalized() 
+		var desired_velocity =  direction * SPEED
+		var steering = (desired_velocity - velocity) * delta * 2.5
+		velocity += steering
+		move_and_slide()
 
 
 func _facing() -> void:
@@ -43,3 +53,16 @@ func _facing() -> void:
 func _on_timer_timeout() -> void:
 	if hp_bar.get_hp() > 0:
 		_lp()
+
+
+#############################################################
+## Signals
+#############################################################
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		is_player_in_range = true
+
+
+func _on_attack_range_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		is_player_in_range = false
