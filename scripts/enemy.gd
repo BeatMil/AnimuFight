e98e1781102c90d0972172adde296a37c97ec0ca
beatmil <anutorn@gmail.com>
2024-09-08 21:49:13 +0100

    Fix bug enemy won't correct direction when attack

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -21,12 +21,18 @@ func _physics_process(_delta: float) -> void:
 	is_face_right = not sprite_2d.flip_h
 	_z_index_equal_to_y()
 	_move(_delta)
-	_facing()
+	if state == States.IDLE:
+		_facing()
 
 	if is_player_in_range_lp:
+		velocity = Vector2.ZERO
 		_lp()
 	elif is_player_in_range_attack01:
+		velocity = Vector2.ZERO
 		_attack01()
+
+	## debug
+	$DebugLabel.text = "%s"%velocity
 
 
 func _ready() -> void:
@@ -60,8 +66,10 @@ func _lp() -> void:
 func _facing() -> void:
 	if velocity.x > 0 :
 		sprite_2d.flip_h = false
-	else:
+	elif velocity.x < 0 :
 		sprite_2d.flip_h = true
+	elif velocity.x == 0:
+		pass
 
 
 func _on_timer_timeout() -> void:
@@ -72,29 +80,38 @@ func _on_timer_timeout() -> void:
 #############################################################
 ## Signals
 #############################################################
-func _on_attack_range_body_entered(body: Node2D) -> void:
+func _on_lp_range_r_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		sprite_2d.flip_h = false
 		is_player_in_range_lp = true
 
 
-func _on_attack_range_body_exited(body: Node2D) -> void:
+func _on_lp_range_l_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		is_player_in_range_lp = false
+		sprite_2d.flip_h = true
+		is_player_in_range_lp = true
 
 
-func _on_attack_range_01r_body_entered(body: Node2D) -> void:
+func _on_attack_01_range_r_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		var result = randi() % 2  # This will give either 0 or 1 randomly.
+		sprite_2d.flip_h = false
+		is_player_in_range_attack01 = true
 
-		if result == 0:
-			is_player_in_range_attack01 = true
-		else:
-			is_player_in_range_attack01 = false
+
+func _on_attack_01_range_l_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		sprite_2d.flip_h = true
+		is_player_in_range_attack01 = true
 
 
 func _on_attack_range_01r_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		is_player_in_range_attack01 = false
+
+
+func _on_lp_range_r_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		is_player_in_range_lp = false
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
