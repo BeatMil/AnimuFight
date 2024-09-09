e6b8bfbfcbb08da2da2e41d9b9b0f2661a2c8ca9
beatmil <anutorn@gmail.com>
2024-09-09 23:10:58 +0100

    Learn how to autoload scenes
    Instead of using SceneTree.change_scene_path()
    we can just add scene to root by get_tree().root.add_child() then set
    the current scene then remove the scene

---
+++ scripts/test.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+const PLAYER = preload("res://scenes/test2.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	print(get_tree().current_scene.name)
+	var new_scene = PLAYER.instantiate()
+	get_tree().root.add_child(new_scene)
+	get_tree().current_scene = new_scene
