396e812b836cb0acff0214d5ff11f14831d9062d
beatmil <anutorn@gmail.com>
2024-09-21 11:39:43 +0100

    Remove debug print

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -4,4 +4,3 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioServer.set_bus_volume_db(0, -7)
-	print(AudioServer.get_bus_volume_db(0))
