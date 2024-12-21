9e301eb5f35c0090e82f441fc176c36ead861438
lonceslash <anutorn@gmail.com>
2024-12-21 01:50:56 +0000

    enemy_spawner can spawn random enemy from array

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
-const ENEMY = preload("res://nodes/enemy.tscn")
 @onready var enemy_count: Node = $enemyCount
 @export var target: CharacterBody2D
-@export var enemy_to_spawn: Resource
+@export var enemy_to_spawn: Array
 @export var is_active: bool = true
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +11,8 @@ func _ready() -> void:
 
 
 func _spawn_enemy() -> void:
-	var e = enemy_to_spawn.instantiate()
+	var bob = enemy_to_spawn.pick_random()
+	var e = bob.instantiate()
 	e.position = self.position
 	e.target = target
 	enemy_count.add_child(e)
