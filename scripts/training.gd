bd501836464ac87810ae567d95172d24385cc1b4
lonceslash <anutorn@gmail.com>
2025-02-16 03:08:25 +0700

    Add heavy atk to training

--- scripts/training.gd
+++ scripts/training.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 @onready var light_atk_label: Label = $CanvasLayer/LightAtkLabel
-
+@onready var heavy_atk_label: Label = $CanvasLayer/HeavyAtkLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +17,5 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("lp"):
 		light_atk_label.turn_green()
+	if event.is_action_pressed("hp"):
+		heavy_atk_label.turn_green()
