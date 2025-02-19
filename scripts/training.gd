66650c1c04f66cfb7262eac761fbe59f994f47ba
lonceslash <anutorn@gmail.com>
2025-02-19 13:49:55 +0700

    Add block label in training mode

--- scripts/training.gd
+++ scripts/training.gd
@@ -1,17 +1,10 @@
 extends Node2D
 
+
 @onready var player: CharacterBody2D = $Player
 @onready var light_atk_label: Label = $CanvasLayer/LightAtkLabel
 @onready var heavy_atk_label: Label = $CanvasLayer/HeavyAtkLabel
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@onready var block_label: Label = $CanvasLayer/BlockLabel
 
 
 func _input(event: InputEvent) -> void:
@@ -19,3 +12,5 @@ func _input(event: InputEvent) -> void:
 		light_atk_label.turn_green()
 	if event.is_action_pressed("hp"):
 		heavy_atk_label.turn_green()
+	if event.is_action_pressed("block"):
+		block_label.turn_green()
