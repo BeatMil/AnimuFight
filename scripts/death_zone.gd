2d5e82038456ede0177e69eeeb0bb5be60c78087
lonceslash <anutorn@gmail.com>
2025-02-23 15:05:19 +0700

    Fix deathzone not kill air object

--- scripts/death_zone.gd
+++ scripts/death_zone.gd
@@ -3,18 +3,17 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("hitted"):
-		print("===deathzone===")
-		if body.is_on_floor():
-			body.hitted(
-			self,
-			true,
-			Vector2.ZERO,
-			0,
-			0,
-			0,
-			Vector2(30, 0.5),
-			9999,
-			Enums.Attack.UNBLOCK,
-			Vector2(0.8, 0.8),
-			0
-			)
+		print_rich("[color=orange][b]===DeathZone===[/b][/color]")
+		body.hitted(
+		self,
+		true,
+		Vector2.ZERO,
+		0,
+		0,
+		0,
+		Vector2(30, 0.5),
+		9999,
+		Enums.Attack.UNBLOCK,
+		Vector2(0.8, 0.8),
+		0
+		)
