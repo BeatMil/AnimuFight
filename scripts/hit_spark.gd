528c8c2b50e957c797f996362b84590e38f622af
lonceslash <anutorn@gmail.com>
2025-04-03 16:55:04 +0700

    Queue free hitspark when it's done

--- scripts/hit_spark.gd
+++ scripts/hit_spark.gd
@@ -4,6 +4,8 @@ extends Node2D
 func _ready() -> void:
 	$GPUParticles2D.one_shot = true
 	$GPUParticles2D.emitting = true
+	await get_tree().create_timer(5).timeout
+	queue_free()
 
 
 func _on_gpu_particles_2d_finished() -> void:
