503c87d34773f08fd458197294026fdcdbbd29de
lonceslash <anutorn@gmail.com>
2025-05-01 19:26:48 +0700

    Remove prints

--- scripts/attack_queue.gd
+++ scripts/attack_queue.gd
@@ -9,7 +9,7 @@ var can_attack: bool = false
 func start_queue_timer():
 	can_attack = false
 	attack_queue_timer.start()
-	print_rich("[color=yellow][b]run: wait 1 sec[/b][/color]")
+	# print_rich("[color=yellow][b]run: wait 1 sec[/b][/color]")
 
 
 func stop_queue_timer():
@@ -20,4 +20,4 @@ func stop_queue_timer():
 func _on_attack_queue_timer_timeout() -> void:
 	can_attack = true
 	attack_queue_timer.stop()
-	print_rich("[color=green][b]run: can_attack![/b][/color]", can_attack)
+	# print_rich("[color=green][b]run: can_attack![/b][/color]", can_attack)
