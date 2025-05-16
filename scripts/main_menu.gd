d82ea447141aa7fdb4c1fb0a7aea5041cee8baeb
lonceslash <anutorn@gmail.com>
2025-05-16 09:37:26 +0700

    MainMenu now uses new buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,18 @@
 extends Control
 @onready var menu_button: MenuButton = $CanvasLayer/Option/MenuButton
 @onready var option: Control = $CanvasLayer/Option
+@onready var start_button: Button = $CanvasLayer/VBoxContainer/StartButton
 
 
 var resolution = {
 	0: Vector2(1920, 1080),
 	1: Vector2(800, 600),
-
 }
 
 
 func _ready() -> void:
+	start_button.grab_focus()
+	%MenuCursor.move_to(Vector2(160, 768))
 	print(DisplayServer.window_get_size())
 	# id_pressed
 	menu_button.get_popup().id_pressed.connect(change_resolution)
@@ -29,13 +31,13 @@ func change_resolution(id: int) -> void:
 	print(DisplayServer.window_get_size())
 
 
-func _on_button_2_button_down() -> void:
+func _on_start_button_pressed() -> void:
+	SceneTransition.change_scene("res://scenes/intro.tscn")
+
+
+func _on_option_button_pressed() -> void:
 	option.visible = !option.visible
 
 
-func _on_button_3_button_down() -> void:
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_button_start_button_down() -> void:
-	SceneTransition.change_scene("res://scenes/intro.tscn")
