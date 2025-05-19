b095c59ad5ceb58397bed07b65342fac66d6a309
beatmil <anutorn@gmail.com>
2025-05-19 13:26:30 +0700

    Add skip_bar to intro

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -3,6 +3,10 @@ extends Node2D
 
 var SSR_LABEL = preload("res://nodes/ssr_label.tscn")
 var NEXT = preload("res://scenes/intro_2.tscn")
+@onready var skip_bar: ProgressBar = $CanvasLayer/SkipBar
+var is_skipping: bool = false
+var is_fully_visible: bool = false
+@onready var skip_bar_player: AnimationPlayer = $CanvasLayer/SkipBar/SkipBarPlayer
 
 
 func _spawn_label_1(_label: String) -> void:
@@ -43,6 +47,22 @@ func _spawn_label_6(_label: String) -> void:
 	add_child(label)
 
 
+func _physics_process(delta: float) -> void:
+	if is_skipping:
+		skip_bar.value += skip_bar.step
+	else:
+		skip_bar.value -= skip_bar.step
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		is_skipping = true
+		skip_bar_player.play("fade_in")
+	elif Input.is_action_just_released("ui_cancel"):
+		is_skipping = false
+		skip_bar_player.play_backwards("fade_in")
+
+
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	match anim_name:
 		"a":
@@ -51,3 +71,14 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 			$AnimationPlayer.play("c")
 		"c":
 			get_tree().change_scene_to_file("res://scenes/intro_2.tscn")
+
+
+func _on_skip_bar_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in":
+		print("fade_in: fishined!")
+		is_fully_visible = true
+
+
+func _on_skip_bar_value_changed(value: float) -> void:
+	if skip_bar.value == skip_bar.max_value:
+		SceneTransition.change_scene("res://scenes/stage01.tscn")
