4da64077cf8538b6bbdc9d0256e0c0d326bed756
beatmil <anutorn@gmail.com>
2025-05-29 18:08:19 +0700

    Enemy1 and Enemy2 can gain back health when not executed

--- scripts/enemy_02.gd
+++ scripts/enemy_02.gd
@@ -118,7 +118,9 @@ func _on_lp_range_r_body_exited(body: Node2D) -> void:
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	# if anim_name in ["lp1", "attack01_1", "hitted", "down"]:
+	if anim_name == "execute":
+		animation_player.play("idle")
+		hp_bar.hp_up(5)
 	if anim_name in ["lp1", "attack01_1", "lp1_chain"]:
 		state = States.IDLE
 		animation_player.play("idle")
