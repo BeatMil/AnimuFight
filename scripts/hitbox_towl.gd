1392a53b64d99113e6c454bc24f1421f2648af8b
beatmil <anutorn@gmail.com>
2025-07-14 20:13:37 +0700

    New throw system now works! Yay (I hope XD)

--- scripts/hitbox_towl.gd
+++ scripts/hitbox_towl.gd
@@ -3,7 +3,44 @@ extends "res://scripts/hitbox.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("hitbox towl desu!")
 	pass
 	# timer.wait_time = time_left_before_queue_free
 	# timer.start()
 	# _set_collision_hit_player()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("hitted"):
+		body.hitted(get_parent(),
+		get_parent().is_face_right,
+		push_power_ground,
+		push_type_ground,
+		hitlag_amount_ground,
+		hitstun_amount_ground,
+		screenshake_amount,
+		damage,
+		type,
+		zoom,
+		zoom_duration,
+		slow_mo_on_block
+		)
+		_on_timer_timeout()
+	print(body.name)
+	await get_tree().create_timer(0.5).timeout
+	if body.state not in [17, 5]: #THROW_BREAKABLE:
+		return
+	body.hitted(get_parent(),
+	get_parent().is_face_right,
+	push_power_ground,
+	push_type_ground,
+	hitlag_amount_ground,
+	hitstun_amount_ground,
+	screenshake_amount,
+	damage,
+	Enums.Attack.NORMAL,
+	zoom,
+	zoom_duration,
+	slow_mo_on_block
+	)
+	_play_hit_random_pitch()
