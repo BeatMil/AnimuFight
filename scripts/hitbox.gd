7e70cc2fa7669c4fa645fb06999a65b658b79405
BeatMil <anutorn@gmail.com>
2025-09-12 15:43:14 +0700

    Fix wrong block sfx when blockstunned

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -128,6 +128,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 	if body.state not in [
 		body.States.BLOCK,
+		body.States.BLOCK_STUNNED,
 		body.States.PARRY,
 		body.States.ARMOR,
 		body.States.IFRAME,
