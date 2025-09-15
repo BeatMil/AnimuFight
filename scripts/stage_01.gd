27198791792017e3156420bb0cd197ea42140d3d
BeatMil <anutorn@gmail.com>
2025-09-15 20:02:05 +0700

    Make first sceenlock of stage_01

--- scripts/stage_01.gd
+++ scripts/stage_01.gd
@@ -2,13 +2,18 @@ extends Node2D
 
 
 @onready var animation_player: AnimationPlayer = $MangoBossSitBanana/AnimationPlayer
+@onready var area_lock_player: AnimationPlayer = $AreaLockPlayer
+@onready var enemy_spawner_new: Node2D = $EnemySpawnerNew
 
 
 func _ready() -> void:
 	# Set camera lock
 	# get_node_or_null("Player/Camera").set_screen_lock(0, 1920, 135, 1129)
 	get_node_or_null("Player/Camera").set_screen_lock(-10000000, 10000000, -10000000, 1000)
+	# get_node_or_null("Player/Camera").set_screen_lock(-1920, 1920, -10000000, 1000)
 	get_node_or_null("Player/Camera").set_zoom(Vector2(1,1))
+	area_lock_player.play("RESET")
+	enemy_spawner_new.is_active = false
 
 	Settings.current_stage = "res://scenes/stage_01.tscn"
 
@@ -26,3 +31,9 @@ func _ready() -> void:
 
 func _on_market_green_banana_fly() -> void:
 	animation_player.play("shock")
+
+
+func _on_area_1_lock_trigger_body_entered(_body: Node2D) -> void:
+	area_lock_player.play("1_in")
+	get_node_or_null("Player/Camera").set_screen_lock(-1920, 1920, -10000000, 1000)
+	enemy_spawner_new.is_active = true
