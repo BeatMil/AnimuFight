e2bd3cf17ecc49aff5e9cfc4279083c0b34c99b7
BeatMil <anutorn@gmail.com>
2025-10-01 14:26:38 +0700

    Fix pre_stage_01 keep zoom when restart

--- scripts/pre_stage_01.gd
+++ scripts/pre_stage_01.gd
@@ -32,7 +32,8 @@ func _ready() -> void:
 	CameraManager.enable_all_camera()
 	CameraManager.player = player
 	CameraManager.make_current(0)
-	CameraManager.zoom_permanent(Vector2(0.1, 0.1))
+	# CameraManager.zoom_permanent(Vector2(1.9, 0.9))
+	CameraManager.set_zoom(Vector2(1, 1))
 	CameraManager.set_screen_lock(0, 1940, 135, 1029)
 	print("_ready: _pre_stage_01.gd")
 	# CameraManager.set_screen_lock(0, 1920, 0, 1000)
